1c8041bdb9ae10f8841908665978b118949eead8
LSchaumburg <luke.schaumburg@my.jcu.edu.au>
2015-11-04 12:24:32 +1000

    get_all_details module added

--- currency.py
+++ currency.py
@@ -1,6 +1,7 @@
-__author__ = 'Luke'
+__author__ = 'Luke Schaumburg'
 
 import web_utility
+
 
 # convert will access the web utility
 def convert(amount, original_currency_code, destination_currency_code):
@@ -40,13 +41,24 @@ def get_details(country_name):
             if country_line[0] != country_name:
                 return ()
             else:
-                return country_details.__add__((country_line[0], country_line[1], country_line[2]))
+                return country_details.__add__((country_line[0], country_line[1], country_line[2].strip("\n")))
 
     # print(country_details)
 
     countries_file.close()
     return ()
 
+
+def get_all_details():
+    countries_file = open("currency_details.txt", "r", encoding='utf-8')
+
+    all_details = {}
+
+    for line in countries_file:
+        country_line = line.split(",")
+        all_details[str(country_line[0])] = ",".join(country_line).strip("\n")
+
+    return all_details
 # get only the line that the user refers to (eg. "Australia" only reads Australia,AUD,$
 
 # print(convert(1, "AUD", "JPY"))
@@ -56,25 +68,26 @@ def get_details(country_name):
 """ Code Testing """
 if __name__ == "__main__":
 
-    print("invalid conversion", "1", "AUD", "->", "AUD", "=", currency.convert(1, "AUD", "AUD"))
-    print("invalid conversion", "1", "JPY", "->", "ABC", "=", currency.convert(1, "JPY", "ABC"))
-    print("invalid conversion", "1", "ABC", "->", "USD", "=", currency.convert(1, "ABC", "USD"))
-    print("valid conversion", "10.95", "AUD", "->", "JPY", "=", currency.convert(10.95, "AUD", "JPY"))
-    print("valid conversion reverse", "965.71", "JPY", "->", "AUD", "=", currency.convert(965.71, "JPY", "AUD"))
-    print("valid conversion", "10.95", "AUD", "->", "BGN", "=", currency.convert(10.95, "AUD", "BGN"))
-    print("valid conversion reverse", "13.82", "BGN", "->", "AUD", "=", currency.convert(13.82, "BGN", "AUD"))
-    print("valid conversion", "200.15", "BGN", "->", "JPY", "=", currency.convert(200.15, "BGN", "JPY"))
-    print("valid conversion reverse", "13390.51", "JPY", "->", "BGN", "=", currency.convert(13390.51, "JPY", "BGN"))
-    print("valid conversion", "100", "JPY", "->", "USD", "=", currency.convert(100, "JPY", "USD"))
-    print("valid conversion reverse", "0.83", "USD", "->", "JPY", "=", currency.convert(0.83, "USD", "JPY"))
-    print("valid conversion", "19.99", "USD", "->", "BGN", "=", currency.convert(19.99, "USD", "BGN"))
-    print("valid conversion reverse", "34.39", "BGN", "->", "USD", "=", currency.convert(34.39, "BGN", "USD"))
-    print("valid conversion", "19.99", "USD", "->", "AUD", "=", currency.convert(19.99, "USD", "AUD"))
-    print("valid conversion reverse", "27.26", "AUD", "->", "USD", "=", currency.convert(27.26, "AUD", "USD"))
-
-    print("invalid details", currency.get_details("Unknown"))
-    print("invalid details", currency.get_details("Japanese"))
-    print("invalid details", currency.get_details(""))
-    print("valid details", currency.get_details("Australia"))
-    print("valid details", currency.get_details("Japan"))
-    print("valid details", currency.get_details("Hong Kong"))
+    # print("invalid conversion", "1", "AUD", "->", "AUD", "=", convert(1, "AUD", "AUD"))
+    # print("invalid conversion", "1", "JPY", "->", "ABC", "=", convert(1, "JPY", "ABC"))
+    # print("invalid conversion", "1", "ABC", "->", "USD", "=", convert(1, "ABC", "USD"))
+    # print("valid conversion", "10.95", "AUD", "->", "JPY", "=", convert(10.95, "AUD", "JPY"))
+    # print("valid conversion reverse", "965.71", "JPY", "->", "AUD", "=", convert(965.71, "JPY", "AUD"))
+    # print("valid conversion", "10.95", "AUD", "->", "BGN", "=", convert(10.95, "AUD", "BGN"))
+    # print("valid conversion reverse", "13.82", "BGN", "->", "AUD", "=", convert(13.82, "BGN", "AUD"))
+    # print("valid conversion", "200.15", "BGN", "->", "JPY", "=", convert(200.15, "BGN", "JPY"))
+    # print("valid conversion reverse", "13390.51", "JPY", "->", "BGN", "=", convert(13390.51, "JPY", "BGN"))
+    # print("valid conversion", "100", "JPY", "->", "USD", "=", convert(100, "JPY", "USD"))
+    # print("valid conversion reverse", "0.83", "USD", "->", "JPY", "=", convert(0.83, "USD", "JPY"))
+    # print("valid conversion", "19.99", "USD", "->", "BGN", "=", convert(19.99, "USD", "BGN"))
+    # print("valid conversion reverse", "34.39", "BGN", "->", "USD", "=", convert(34.39, "BGN", "USD"))
+    # print("valid conversion", "19.99", "USD", "->", "AUD", "=", convert(19.99, "USD", "AUD"))
+    # print("valid conversion reverse", "27.26", "AUD", "->", "USD", "=", convert(27.26, "AUD", "USD"))
+    #
+    # print("invalid details", get_details("Unknown"))
+    # print("invalid details", get_details("Japanese"))
+    # print("invalid details", get_details(""))
+    # print("valid details", get_details("Australia"))
+    # print("valid details", get_details("Japan"))
+    # print("valid details", get_details("Hong Kong"))
+    # print(get_all_details())
