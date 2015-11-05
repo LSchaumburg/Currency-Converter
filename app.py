35485575b06aaeb342d520437d052aaa39385488
LSchaumburg <luke.schaumburg@my.jcu.edu.au>
2015-11-05 14:29:39 +1000

    Update app.py
    Completed conversion function. Working on getting the app fully functional.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-__author__ = 'jc247746'
+__author__ = 'Luke Schaumburg'
 
 # An __init__ method capable of initialising a trip details field
 # A build method that sets the window title appropriately, sets the window size to 350 x 700, and defines the root widget appropriately
@@ -7,12 +7,10 @@ __author__ = 'jc247746'
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-from datetime import datetime
+import time
 import currency
 from trip import Details
-from currency import web_utility
-from kivy.properties import StringProperty
-from kivy.properties import ListProperty
+from trip import Error
 
 
 class ConverterApp(App):
@@ -20,59 +18,65 @@ class ConverterApp(App):
 
     home_country = trip_file.readline().strip("\n")
 
-    date_today = datetime.now().strftime("%Y/%m/%d")
+
+    date_today = time.strftime("%Y/%m/%d")
     current_date = "Today is: \n" + date_today
+
+    saved_trips = []
 
     for line in trip_file:
         parts = line.split(",")
         country_name = parts[0]
-        # print(country_name)
+        saved_trips.append(country_name)
         start_date = parts[1]
-        # print(start_date)
         end_date = parts[2]
-        # print(end_date)
         if start_date <= date_today <= end_date:
             current_country = country_name
 
-    # COUNTRY NAME TO BE BASE OFF THE CURRENT DATE AND THE DATES IN THE TRIP FILE
-    # part = date_today.split("/")
-    # for line in trip_file:
-    #     if part[1] <= date_today <= part[2]:
-    #         current_country_name = part[0]
-    current_location = "Current trip location:\n" + current_country
+    trip = Details()
+    for line in trip_file:
+        sections = line.strip("\n").split(",")
+        print(sections)
+        try:
+            trip.add(sections[0], sections[1], sections[2])
+        except Error as error:
+            print(error.value)
 
-    current_place = StringProperty()
-    country_codes = ListProperty()
+    current_location = "Current trip location:\n" + trip.current_country(date_today)
 
-    # trip_details = currency.get_all_details()
-    # trip_details.keys() is for the country code
-    # home_country is for home country_name
-
-    def __init__(self):  # , trip_details):
+    def __init__(self):
         super().__init__()
+        self.trip_details = currency.get_all_details()
+        # print(self.trip_details)
 
     def build(self):
-        # __init__.trip_details = currency.get_all_details()
-        # print(listed_trips)
-
         Window.size = (350, 700)
         self.title = "Foreign Exchange Calculator"
         self.root = Builder.load_file('gui.kv')
         self.accepted_trip_details()
-        # self.country_names = sorted(listed_trips)
-        # self.current_place = self.country_names[0]
         return self.root
+
+    def disable_app(self):
+        self.root.ids.status_field.disabled = True
+        self.root.ids.home_currency_input.disabled = True
+        self.root.ids.away_currency_input.disabled = True
+        self.root.ids.country_selector.disabled = True
+
+    def enable_app(self):
+        self.root.ids.status_field.disabled = False
+        self.root.ids.home_currency_input.disabled = False
+        self.root.ids.away_currency_input.disabled = False
+        self.root.ids.country_selector.disabled = False
 
     def accepted_trip_details(self):
         # how to disable app from being used when an error occurs???
-        trip_details = currency.get_all_details()
+        trip_details = self.trip_details
         list_of_trips = sorted(trip_details.keys())
         # print(list_of_trips)
         app_config_file = open("config.txt", "r", encoding='utf-8')
         first_line = app_config_file.readline()
-        print(first_line.strip("\n"))
-        # for country in list_of_trips:
-            # print(list_of_trips)
+        # print(first_line.strip("\n"))
+
         if first_line.strip("\n") in list_of_trips:
             for line in app_config_file:
                 parts = line.split(",")
@@ -81,72 +85,61 @@ class ConverterApp(App):
                         self.root.ids.status_field.text = "Valid country"
                     else:
                         self.root.ids.status_field.text = "Invalid trip details"
+                        self.disable_app()
                 else:
                     self.root.ids.status_field.text = "Invalid trip details\ninvalid country"
+                    self.disable_app()
         else:
             self.root.ids.status_field.text = "Invalid home country"
+            self.disable_app()
         app_config_file.close()
 
     def handle_update_button(self):
-        localtime = datetime.now().strftime("%H:%M:%S")
+        localtime = time.strftime("%H:%M:%S")
+        self.enable_app()
         self.root.ids.status_field.text = "Last updated: " + localtime
+        self.root.ids.country_selector.text = current_country
+        self.handle_currency_input("away")
 
-        print("Updated exchange rate")
+    # def change_country(self):
+        # print(self.root.ids.country_selector.text)
+        # country_information = self.trip_details
+        # selected_country = self.root.ids.country_selector.text
+        # away_country_code = (country_information.get(selected_country)[1])
 
-    def handle_home_currency_input(self):
-        currency_value = self.root.ids.currency_value.text
-
-        for parts in trip_details:
-            if parts.key() == home_country:
-                home_code = parts[2]
-
-        for parts in trip_details:
-            if parts.keys() == country_selector:
-                away_code = parts[2]
-
-        print(currency.convert(currency_value, home_code, away_code))
-
-        # self.root.ids.status_field.text = home_code + "(" + home_symbol + ") to " + away_code + "(" + away_symbol + ")"
-
-        ## input_url = "https://www.google.com/finance/converter?a={}&from={}&to={}".format(currency_value, home_code, away_code)
-        ## converted_value = web_utility.load_page(input_url)
-        ## result_amount_start = converted_value.find("class=bld>")
-        ## result_amount_finish = converted_value.find("</span")
-        ## result_amount_location = converted_value[result_amount_start:result_amount_finish]
-        ## # print(result_amount_location)
-        ##
-        ## result_amount = result_amount_location.split("class=bld>")
-        ## split_amount = result_amount[1]
-        ## final_value = split_amount.split(" ")
-        ## self.root.ids.current_currency_input.text = final_value[0]
-
-    # def handle_away_currency_input(self, currency_value, home_country_code, away_country_code):
-    #     currency.convert(currency_value, away_country_code, home_country_code)
-    #     self.root.ids.status_field.text = "{}, ({}), to {}, ({})".format(away_country_code, away_symbol, home_country_code, home_symbol)
-    #     self.root.ids.home_currency_input = converted_value
-
-    def change_country(self):
-        self.root.ids.country_selector.text = LISTED_TRIPS[country_code]
-        # listed_trips = []
+        # saved_trips = []
         #
+        # trip_file = open("config.txt", 'r', encoding='utf-8')
         # for line in trip_file:
         #     parts = line.split(",")
         #     country_name = parts[0]
-        #     listed_trips.append(country_name)
-        # # print(listed_trips)
-        # self.root.ids.country_selector.text = listed_trips[0]
+        #     saved_trips.append(country_name)
+        # self.root.ids.country_selector.text = saved_trips[0]
+        #
+        # trip_file.close()
 
+    def handle_currency_input(self, home_away):
+        country_information = self.trip_details
+        selected_country = self.root.ids.country_selector.text
 
-# for updating the app:
-# value_as_string = str(currency_value)
+        away_country_information = country_information.get(selected_country)
+        away_country_code = (away_country_information[1])
 
-# currency from home to away
-# input_url = ("https://www.google.com/finance/converter?a={}&from={}&to={}").format(value_as_string, home_country_code, away_country_code)
+        home_country_information = (country_information.get(self.root.ids.home_country.text.strip("\n")))
+        home_country_code = home_country_information[1]
 
-# currency from away to home
-# input_url = ("https://www.google.com/finance/converter?a={}&from={}&to={}").format(value_as_string, away_country_code, home_country_code)
+        if home_away == "away":
+            currency_value = self.root.ids.away_currency_input.text
 
-# converted_value = web_utility.load_page(input_url)
-# return converted_value
+            converted_value = currency.convert(currency_value, away_country_code, home_country_code)
+            self.root.ids.home_currency_input.text = str(converted_value)
+            self.root.ids.status_field.text = away_country_code + " (" + away_country_information[2] + ") to " + home_country_code + " (" + home_country_information[2] + ")"
+
+        elif home_away == "home":
+            currency_value = self.root.ids.home_currency_input.text
+
+            converted_value = currency.convert(currency_value, home_country_code, away_country_code)
+            self.root.ids.away_currency_input.text = str(converted_value)
+            self.root.ids.status_field.text = home_country_code + " (" + home_country_information[2] + ") to " + away_country_code + " (" + away_country_information[2] + ")"
 
 ConverterApp().run()
